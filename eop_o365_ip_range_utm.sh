e9bba2c2ec5084d2e65f837a2c0fbde481c45444
iaasteamtemplates <iaasteamtemplates@users.noreply.github.com>
2017-11-16 11:07:16 +0000

    Add files via upload

---
+++ eop_o365_ip_range_utm.sh
@@ -0,0 +1,51 @@
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop1', 'address' => '23.103.132.0', 'netmask' => '22', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-1' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop2', 'address' => '23.103.136.0', 'netmask' => '21', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-2' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop3', 'address' => '23.103.144.0', 'netmask' => '20', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-3' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop4', 'address' => '23.103.198.0', 'netmask' => '23', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-4' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop5', 'address' => '23.103.200.0', 'netmask' => '22', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-5' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop6', 'address' => '23.103.212.0', 'netmask' => '22', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-6' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop7', 'address' => '40.92.0.0', 'netmask' => '14', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-7' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop8', 'address' => '40.107.0.0', 'netmask' => '17', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-8' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop9', 'address' => '40.107.128.0', 'netmask' => '18', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-9' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop10', 'address' => '52.100.0.0', 'netmask' => '14', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-10' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop11', 'address' => '65.55.88.0', 'netmask' => '24', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-11' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop12', 'address' => '65.55.169.0', 'netmask' => '24', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-12' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop13', 'address' => '94.245.120.64', 'netmask' => '26', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-13' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop14', 'address' => '104.47.0.0', 'netmask' => '17', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-14' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop15', 'address' => '134.170.132.0', 'netmask' => '24', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-15' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop16', 'address' => '134.170.140.0', 'netmask' => '24', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-16' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop17', 'address' => '157.55.234.0', 'netmask' => '24', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-17' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop18', 'address' => '157.56.110.0', 'netmask' => '23', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-18' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop19', 'address' => '157.56.112.0', 'netmask' => '24', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-19' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop20', 'address' => '207.46.51.64', 'netmask' => '26', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-20' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop21', 'address' => '207.46.100.0', 'netmask' => '24', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-21' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop22', 'address' => '207.46.163.0', 'netmask' => '24', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-22' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop23', 'address' => '213.199.154.0', 'netmask' => '24', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-23' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop24', 'address' => '213.199.180.128', 'netmask' => '26', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-24' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'network', 'data' => { '_ref' => 'REF_NetNetEop25', 'address' => '216.32.180.0', 'netmask' => '23', 'address6' => '', 'comment' => '', 'duids' => [], 'hostnames' => [], 'interface' => '', 'name' => 'eop-25' } }"
+
+cc set_object "{ 'class' => 'network', 'type' => 'group', 'data' => { 'comment' => '', 'members' => [ 'REF_NetNetEop1', 'REF_NetNetEop2', 'REF_NetNetEop3', 'REF_NetNetEop4', 'REF_NetNetEop5', 'REF_NetNetEop6', 'REF_NetNetEop7', 'REF_NetNetEop8', 'REF_NetNetEop9', 'REF_NetNetEop10', 'REF_NetNetEop11', 'REF_NetNetEop12', 'REF_NetNetEop13', 'REF_NetNetEop14', 'REF_NetNetEop15', 'REF_NetNetEop16', 'REF_NetNetEop17', 'REF_NetNetEop18', 'REF_NetNetEop19', 'REF_NetNetEop20', 'REF_NetNetEop21', 'REF_NetNetEop22', 'REF_NetNetEop23', 'REF_NetNetEop24', 'REF_NetNetEop25' ], 'types' => [ 'network' ], 'name' => 'eop-range' } }"
